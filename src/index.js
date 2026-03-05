7bc49244c8568a909799e8a426c6b3523ef12638
anthonyhem <31942622+anthonyhem@users.noreply.github.com>
2026-03-05 16:57:59 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Anthony Has Left the Internet</h1>
+      <p>Please enjoy this instead:</p>
+      <video
+        controls
+        width="90%"
+        max-width="600"
+        src="video.mp4"
+      >
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
